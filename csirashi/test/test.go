c15c4e4aa26edaa0a15197859d61b5c74403522c
Bryan <70968819+brykumara@users.noreply.github.com>
2022-08-01 07:53:28 +0100

    Add files via upload

--- csirashi/test/test.go
+++ csirashi/test/test.go
@@ -5,14 +5,19 @@ import (
 	"fmt"
 
 	"github.com/brykumara/circlclone/csidh"
+	"github.com/cloudflare/circl/dh/csidh"
+)
+
+const (
+	Lambda  = 128
+	message = 3
 )
 
 var rng = rand.Reader
 
 func main() {
-	GeneratePairs(6)
+	GeneratePairs(message)
 }
-
 func GeneratePairs(message int) { // Create pairs based on (3) of the CSI-RAShi paper
 	var secret csidh.PrivateKey
 	csidh.GeneratePrivateKey(&secret, rng)
@@ -25,6 +30,22 @@ func GeneratePairs(message int) { // Create pairs based on (3) of the CSI-RAShi 
 		csidh.GroupAction(&pub, &secret, rng)
 		GroupActionSet[i] = pub
 	}
-	fmt.Println(Initial_set)
-	fmt.Println(GroupActionSet)
+	fmt.Println("Your initial indexed set is:", Initial_set)
+	fmt.Println("Your set after applying the secret is:", GroupActionSet)
+	fmt.Println("Your secret is:", secret)
+}
+
+func NIZKP(Initial_set []csidh.PublicKey, GroupActionSet []csidh.PublicKey, secret csidh.PrivateKey) {
+	Challenge_set1 := make([]csidh.PublicKey, (message + Lambda))
+	Secretset := make([]csidh.PrivateKey, Lambda)
+	for j := 0; j < Lambda; j++ {
+		var secret2 csidh.PrivateKey
+		csidh.GeneratePrivateKey(&secret2, rng)
+		Secretset[j] = secret2
+		for i := 0; i < message; i++ {
+			csidh.GroupAction(&Initial_set[i+j], &Secretset[j], rng)
+			Challenge_set1[j+i] = Initial_set[i+j]
+		}
+	}
+	// use SHA3 to create output of 128 bit length
 }
