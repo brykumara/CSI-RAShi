2dec2331d840880a202957937c9c6690b2c80867
Bryan <70968819+brykumara@users.noreply.github.com>
2022-08-29 21:52:32 +0100

    Add files via upload

--- csirashi/test/test.go
+++ csirashi/test/test.go
@@ -37,22 +37,22 @@ func SampleSecret(Prime float64) float64 {
 
 func Secret2Vec(secret float64) float64 {
 	Target := make([]float64, csidh.PrimeCount)
-	for i := 0; i < csidh.PrimeCount; i++ {
+	for i := 1; i < csidh.PrimeCount; i++ {
 		Target[i] = 0
 	}
-	Target[0] = secret // Correct
+	Target[0] = secret //
 	B := make([]float64, csidh.PrimeCount*csidh.PrimeCount)
 	// Babai Nearest Plane
-	for i := (csidh.PrimeCount - 1); i >= 73; i-- {
+	for i := (csidh.PrimeCount - 1); i >= 70; i-- { // 74
 		for j := 0; j < len(B); j++ {
 			B[j] = B[j] + (float64)(i*74)
-		} // Correct
-		TargetxB := Innerproduct(Target, B) // Correct -> descends to 0
+		} //
+		TargetxB := Innerproduct(Target, B) //
 		for j := 0; j < len(B); j++ {
 			B[j] = 0
-		} // Need to reset B every time
-		Converted, _ := strconv.ParseFloat(HKZIPStrings[i], 64)                                  // Correct
-		ip1 := new(big.Float).SetPrec(prec).Quo(big.NewFloat(TargetxB), big.NewFloat(Converted)) // Correct -> descends to 0
+		} // Need to reset B every time?
+		HKZIPS, _ := strconv.ParseFloat(HKZIPStrings[i], 64)                                  //
+		ip1 := new(big.Float).SetPrec(prec).Quo(big.NewFloat(TargetxB), big.NewFloat(HKZIPS)) //
 		if ip1.Sign() < 0 {
 			delta := -0.5
 			ip1.Add(ip1, new(big.Float).SetFloat64(delta))
@@ -61,25 +61,30 @@ func Secret2Vec(secret float64) float64 {
 			delta := 0.5
 			ip1.Add(ip1, new(big.Float).SetFloat64(delta))
 		}
-		ip1rounded, _ := ip1.Int(nil) // Correct
-		ip1r := new(big.Float).SetInt(ip1rounded)
-		remainder := new(big.Float).SetPrec(prec).Sub(ip1, ip1r) // Correct
+		ip1int, _ := ip1.Int(nil)
+		ip1floor := new(big.Float).SetInt(ip1int)                    //
+		remainder := new(big.Float).SetPrec(prec).Sub(ip1, ip1floor) //
 		if remainder.Cmp(big.NewFloat(0.5)) > 0 {
-			ip1r = ip1r.Add(ip1r, new(big.Float).SetFloat64(1))
-		}
-		r, _ := ip1r.Float64()
+			ip1floor = ip1floor.Add(ip1floor, new(big.Float).SetFloat64(1))
+		} //
+		r, _ := ip1floor.Float64() // r is actually positive decreasing to 0, so we expect A*r to get smaller and smaller
 		A := make([]float64, csidh.PrimeCount*csidh.PrimeCount)
-		for j := 0; j < len(B); j++ {
+		for j := 0; j < len(A); j++ { // 74*74
 			A[j] = A[j] + (float64)(i*74)
-		} // Correct
-		Sub_Multiple(Target, A, r)
-		//Seems to be a problem when substracting Target by A*r -> converges to negative infinity
-		for j := 0; j < len(A); j++ {
-			A[j] = 0
+			// r is locked for i, and so is i, loop over each element of A first
 		}
-	}
-	Vec := Target
-	Reduce(Vec, 2, 10000)
+		for j := 0; j < len(A); j++ {
+			A[j] = A[j] * r
+		}
+		fmt.Println(A[10])
+		// A*r does descend to 0, so adding them should not be a problem?
+		//Sub_Multiple(Target, A, r)
+		// For this, C seems to be ok. Target is just the value of C?
+
+		//Seems to be a problem when substracting Target by A*r gets too large. Use big float?
+	} //go run test.go hkz.go pool.go norms.go vectors.go
+	//Vec := Target
+	//Reduce(Vec, 2, 10000)
 	return 0
 }
 
